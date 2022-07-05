6d5c83137a00bd8ce64a44879741767ffd88bbf2
HiagoP <hiagoperonip@gmail.com>
2022-07-05 15:34:47 -0300

    Finalizando o trabalho com o botão limpar

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let cor2 = document.querySelector('#cor-2');
 let cor3 = document.querySelector('#cor-3');
 let cor4 = document.querySelector('#cor-4');
 let corEscolhida;
-let pixels = document.querySelector('#pixel-board');
+const pixels = document.querySelector('#pixel-board');
 let botao = document.querySelector('#clear-board');
+const pixel = pixels.childNodes;
 
 function colorSelected(event) {
     let corSelecionada = document.querySelector('.selected');
@@ -18,43 +19,34 @@ cor3.addEventListener('click', colorSelected);
 cor4.addEventListener('click', colorSelected);
 
 function pintarPixel(event2) {
-    let pixel = pixels.children;
-    event2.target.classList.remove('corDeFundoBranca');
+    let clicado = event2.target;
     if (cor1.classList.contains('selected')) {
-        event2.target.classList.remove('corDeFundoBranca');
-        event2.target.classList.remove('corDeFundo2');
-        event2.target.classList.remove('corDeFundo3');
-        event2.target.classList.remove('corDeFundo4');
-        event2.target.classList.add('corDeFundoPreta');
+        event2.target.classList = 'pixel corDeFundoPreta';
     } else if (cor2.classList.contains('selected')) {
-        event2.target.classList.remove('corDeFundoBranca');
-        event2.target.classList.remove('corDeFundoPreta');
-        event2.target.classList.remove('corDeFundo3');
-        event2.target.classList.remove('corDeFundo4');
-        event2.target.classList.add('corDeFundo2');
+        event2.target.classList = 'pixel corDeFundo2';
     } else if (cor3.classList.contains('selected')) {
-        event2.target.classList.remove('corDeFundoBranca');
-        event2.target.classList.remove('corDeFundoPreta');
-        event2.target.classList.remove('corDeFundo2');
-        event2.target.classList.remove('corDeFundo4');
-        event2.target.classList.add('corDeFundo3');
+        event2.target.classList = 'pixel corDeFundo3';
     } else if (cor4.classList.contains('selected')) {
-        event2.target.classList.remove('corDeFundoBranca');
-        event2.target.classList.remove('corDeFundoPreta');
-        event2.target.classList.remove('corDeFundo3');
-        event2.target.classList.remove('corDeFundo2');
-        event2.target.classList.add('corDeFundo4');
+        event2.target.classList = 'pixel corDeFundo4';
     }
 }
 pixels.addEventListener('click', pintarPixel);
 
-function limparTudo() {
-    pixels.children.classList.value = ' ';
-    pixels.children.classList.add('pixel corDeFundoBranca');
+function limparTudo(event3) {
+    for (let i of pixel) {
+        i.classList = 'pixel corDeFundoBranca';
+    }
 }
+// Metodo alternativo usando seus objetos:
+//     for (let i of Object.keys(pixel)) {
+//      let check = pixel[i];
+//      check.classList = 'pixel corDeFundoBranca';
+//   }
+
 botao.addEventListener('click', limparTudo);
 
-window.onload = pintarPixel;
+
+window.onload = pintarPixel, limparTudo;
 
 
 
